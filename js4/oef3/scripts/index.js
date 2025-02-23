0905a62a233e46ad098d4f45c850ab30973d380e
Alexander Vanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-02-23 11:27:20 +0100

    oef 3

--- js4/oef3/scripts/index.js
+++ js4/oef3/scripts/index.js
@@ -1,5 +1,13 @@
 const setup = () => {
-	// deze code wordt pas uitgevoerd als de pagina volledig is ingeladen
+	document.getElementById("button").addEventListener("click", printSplitValue)
+
+}
+
+const printSplitValue = () => {
+	let string = document.getElementById("input").value;
+	let splitString = string.split("").join(" ");
+	document.getElementById("output").textContent = splitString;
+	console.log(splitString);
 }
 
 window.addEventListener("load", setup);
