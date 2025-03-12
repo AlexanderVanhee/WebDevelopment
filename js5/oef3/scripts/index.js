9a9c8449b6f813335ea66001e20cbc11b6fa6f6d
AlexanderVanhee <160625516+AlexanderVanhee@users.noreply.github.com>
2025-03-12 10:19:06 +0100

    Update index.js

--- js5/oef3/scripts/index.js
+++ js5/oef3/scripts/index.js
@@ -6,17 +6,22 @@ const setup = () => {
 
 const addGemeentes = () => {
   const dropdown = document.getElementById("gemeente");
+  let array = [];
   let keepGoing = true;
   while (keepGoing) {
     let gemeente = prompt("Voeg een gemeente toe");
     if (gemeente && gemeente !== "stop") {
-      let option = document.createElement("option");
-      option.text = option.value = gemeente;
-      dropdown.add(option, 0);
+        array.push(gemeente);
     } else {
       keepGoing = false;
     }
   }
+  array.sort().reverse();
+  array.forEach((gemeente) => {
+    let option = document.createElement("option");
+    option.text = option.value = gemeente;
+    dropdown.add(option, 0);
+  })
 };
 
 window.addEventListener("load", setup);
